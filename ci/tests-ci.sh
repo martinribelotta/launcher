2d214eb1a8b706392c153523f519836b7ca31096
Martin Ribelotta <martinribelotta@gmail.com>
2019-06-22 22:42:58 -0300

    Add ci missing directory
    Signed-off-by: Martin Ribelotta <martinribelotta@gmail.com>

---
+++ ci/tests-ci.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -x
+
+source /opt/qt*/bin/qt*-env.sh
+
+set -e
+
+qmake --version
+
+VERSION=$(git rev-parse --short HEAD)
+
+INSTALL_DIR=/tmp/applauncher
+DEPLOY_OPT="-no-translations -verbose=2 -executable=$INSTALL_DIR/usr/bin/applauncher"
+DESKTOP_FILE=$INSTALL_DIR/usr/share/applications/applauncher.desktop
+
+echo ************** LINUX BUILD ***********************
+
+qmake CONFIG+=release CONFIG+=force_debug_info
+make -j4
+make install INSTALL_ROOT=${INSTALL_DIR}
+linuxdeployqt $DESKTOP_FILE $DEPLOY_OPT -appimage
+echo ************** WINDOWS BUILD ***********************
+
+make distclean
+export MXE_PREFIX=i686-w64-mingw32.static
+export MXE=/usr/lib/mxe/usr
+export MXEQT=${MXE}/${MXE_PREFIX}/qt5
+export PATH=${MXE}/bin:${PATH}
+${MXEQT}/bin/qmake CONFIG+=release CONFIG+=force_debug_info embedded-ide.pro
+make -j4
---
+++ ci/tests-environment.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+set -x
+
+sudo add-apt-repository --yes ppa:beineri/opt-qt593-trusty
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+echo "deb http://pkg.mxe.cc/repos/apt trusty main" \
+    | sudo tee /etc/apt/sources.list.d/mxeapt.list
+#echo 'APT::Get::AllowUnauthenticated "true";' \
+##    | sudo tee /etc/apt/apt.conf.d/99myown
+
+sudo apt-get update -qq --allow-unauthenticated
+
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+
+sudo wget https://raw.githubusercontent.com/martinribelotta/pydeployqt/master/deploy.py -O /usr/bin/pydeployqt
+sudo chmod a+x /usr/bin/pydeployqt
+
+MXE=mxe-i686-w64-mingw32.static-
+
+sudo apt-get install  -y --allow-unauthenticated \
+	gcc-8 g++-8 build-essential \
+	qt59base qt59tools qt59svg qt59imageformats qt59x11extras libglu1-mesa-dev \
+	wget fuse \
+	${MXE}gcc ${MXE}g++ \
+	${MXE}qtbase ${MXE}qtsvg ${MXE}qttools ${MXE}qtwinextras ${MXE}qscintilla2
+
+gcc --version
+# sudo update-alternatives --remove-all gcc
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 90 --slave /usr/bin/g++ g++ /usr/bin/g++-8
+gcc --version
+
+cd /tmp/
+wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+sudo cp linuxdeployqt-continuous-x86_64.AppImage /usr/bin/linuxdeployqt
+sudo chmod a+x /usr/bin/linuxdeployqt
+cd -
